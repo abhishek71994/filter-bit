92fd01de89baca9bd4656fb4fbc3005a7a7f3930
Abhishek Prasad <abhishek71994@gmail.com>
2017-07-11 22:49:36 +0530

    search via app-id implemented

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let output=[];
+var loginID;
+var test;
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '111900782773487',
@@ -22,7 +24,9 @@ let output=[];
 
   function statusChangeCallback(response){
   	if (response.status === "connected") {
-  		console.log("We are up and running!");
+      console.log(response);
+      loginID=response.authResponse.userID;
+  		console.log(loginID);
   		setElements(true);
   		testAPI();
   	}else{
@@ -35,49 +39,71 @@ let output=[];
 
 
   	function testAPI(){
-  		FB.api('/me?fields=name,email,posts,birthday',function(response){
+  		FB.api('/'+loginID+'?fields=name,email,posts,birthday',function(response){
   			if(response && !response.error){
   				buildProf(response);
+          document.getElementById('posts').insertAdjacentHTML('beforeend', '<h3>All Posts</h3>');
+          
   			} 
   		});
-  		document.getElementById('posts').insertAdjacentHTML('beforeend', '<h3>All Posts</h3>');
-  		FB.api('/me/feed',doSomething);
-  			
+      test=0;
+  		FB.api('/'+loginID+'/feed',doSomething);
+  		
   	}
 
   	function doSomething(response){
+      console.log(response);
   		if(response && !response.error){
+        if (typeof response.paging === "undefined" || typeof response.paging.next === "undefined" ){
+          document.getElementById('retcheck').innerHTML='<h3 class="heading text-center" id="retcheck">Data retrieving Finished</h3>';
+        }
+        else
+        { 
+          console.log(response);
+          buildFeed(response,test);
+          // test=1;
+          FB.api(response.paging.next, doSomething);
+        }
   				
-  				// if (response.paging.next != "undefined"){
-  					
-  					
-			   //      FB.api(response.paging.next, doSomething);
-			        
-			   //  }
+  		}
+      // else if(response && !response.error &&  (test!==0)){
+      //   if (typeof response.paging === "undefined" || typeof response.paging.next === "undefined" ){
+      //     document.getElementById('retcheck').innerHTML='<h3 class="heading text-center" id="retcheck">Data retrieving Finished</h3>';
+      //   }
+      //   else
+      //   { 
+      //     console.log(response);
+      //     buildFeed(response,test);
+      //     test=1;
+      //     FB.api(response.paging.next, doSomething);
+      //   }
 
-			if (typeof response.paging === "undefined" || typeof response.paging.next === "undefined" ){
-				console.log("data colllected");
-			}
-			else
-			{ 
-				console.log(response);
-				buildFeed(response);
-				FB.api(response.paging.next, doSomething);
-			}
-
+      // }
+      else{     
+			console.log(response.error);
+    }
 
 
   				
   			} 
-  		}
+  		
    
 
-  	function buildFeed(feed){
+  	function buildFeed(feed,test){
   		
   		let feedOut= '';
-  		for(var i=0;i<feed.data.length;i++){
-  			output.push(feed.data[i].message);
-  		}
+      // if(test===0){
+      //   for(var i=0;i<feed.posts.data.length;i++){
+      //   output.push(feed.posts.data[i].message);
+      // }
+      // }
+      //else if(test!==0){
+
+      for(var i=0;i<feed.data.length;i++){
+        output.push(feed.data[i].message);
+      }
+
+      //}
   		output=output.filter(function(data){
   			if(data!=undefined){
   				return data;
@@ -94,14 +120,19 @@ let output=[];
   	}
 
   	function buildProf(user){
+       let profile=`
+        <img src="http://graph.facebook.com/${user.id}/picture">
+        <h3>${user.name}</h3>
+        <h4>${user.email}</h4>
+        
+      `;
+      if (user.birthday !== undefined) {
 
-  		let profile=`
-  			<img src="http://graph.facebook.com/${user.id}/picture">
-  			<h3>${user.name}</h3>
-  			<h4>${user.email}</h4>
-  			<h4>${user.birthday}</h4>
-  			
-  		`;
+      profile+=`
+        <h4>${user.birthday}</h4>
+        
+      `;
+      }
   		// <ul class="list-unstyled list-group">
 	  	// 		<li class="list-group-item">User email:${user.email}<li>
 	  	// 		<li class="list-group-item">User Birthday:${user.birthday}<li>
@@ -124,6 +155,7 @@ let output=[];
 			document.getElementById('logcheck').style.display = 'none';
 			document.getElementById('fb-btn').style.display = 'none';
 			document.getElementById('posts').style.display = 'block';
+      document.getElementById('retcheck').style.display = 'block';
 			document.getElementById('searchPost').style.display = 'block';
 		}else{
 			document.getElementById('profile').style.display = 'none';
@@ -131,6 +163,7 @@ let output=[];
 			document.getElementById('logout').style.display = 'none';
 			document.getElementById('logcheck').style.display = 'block';
 			document.getElementById('posts').style.display = 'none';
+      document.getElementById('retcheck').style.display = 'none';
 			document.getElementById('searchPost').style.display = 'none';
 		}
 	}
